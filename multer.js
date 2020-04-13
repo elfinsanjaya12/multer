52c50717dc57db59fccfbd3c3824616ddbc7d990
Elfin Sanjaya <45805396+elfinsanjaya12@users.noreply.github.com>
2020-04-13 21:05:59 +0700

    Update multer.js

--- multer.js
+++ multer.js
@@ -1,5 +1,31 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+// import uuid from "uuid/v4";
+
+const storageMultiple = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var dir = 'public/images';
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+})
+
+const uploadMultiple = multer({
+  storage: storageMultiple,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).array("image", 12);
+
+
+// Set storage engine
 const storage = multer.diskStorage({
   destination: "public/images",
   filename: function (req, file, cb) {
@@ -15,14 +41,21 @@ const upload = multer({
   }
 }).single("image");
 
+// // Check file Type
 function checkFileType(file, cb) {
+
+  // Allowed ext
   const fileTypes = /jpeg|jpg|png|gif/;
+  // Check ext
   const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
   const mimeType = fileTypes.test(file.mimetype);
+
   if (mimeType && extName) {
     return cb(null, true);
   } else {
     cb("Error: Images Only !!!");
   }
 }
-module.exports = { upload };
+
+module.exports = { uploadMultiple, upload };
